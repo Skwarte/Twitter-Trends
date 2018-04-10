b529990f5df6aaa43298e175e51be7bcd1f85273
skwarte <skwarte@purdue.edu>
2018-04-10 17:51:36 -0400

    removed monkeylearn, using aws instead

--- index.js
+++ index.js
@@ -2,10 +2,14 @@ var express = require('express');
 var app = express();
 var port = process.env.port || 4000;
 var http = require('http').createServer(app);
+
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/home.html');
 });
 
+app.post('/', function(req, res) {
+
+});
 http.listen(port, function() {
     console.log("using port " + port + " \n");
 });
