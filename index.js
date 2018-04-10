f9d7d74d9d37d1ec184f8a43744e04c31798e345
skwarte <skwarte@purdue.edu>
2018-04-10 16:34:03 -0400

    added code to run server

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("testing js");
+var express = require('express');
+var app = express();
+var port = process.env.port || 4000;
+var http = require('http').createServer(app);
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/home.html');
+});
+
+http.listen(port, function() {
+    console.log("using port " + port + " \n");
+});
